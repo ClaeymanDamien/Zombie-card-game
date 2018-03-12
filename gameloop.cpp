5cf0e440d8d2b215db8a298bba023d4077cb3637
DONADEI Emrick <emrick.donadei@hotmail.fr>
2018-03-12 19:36:09 +0100

    Gameloop update
    Ajout des fonctions de base
    
    Ajout des vecteurs deck, defausse, main, ennemis

--- gameloop.cpp
+++ gameloop.cpp
@@ -1,9 +1,37 @@
+#include "Entity.h"
+#include "Card.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Deck cartes
+vector<Card> deck[Tirer,Tirer,Tirer,Tirer,Tirer,Armure,Armure,Armure,Armure,Armure];
+
+// Main du joueur
+vector<Card> main;
+
+// Défausse
+vector<Card> defausse;
+
+vector<Entity> Ennemis;
+
+Entity Player;
+
+Entity player;
 
 void gameloop(){
-
+afficherui();
+draw(5);
+clearconsole();
 }
 
 void afficherui(){
+
+}
+
+void affichermain(){
 
 }
 
@@ -15,7 +43,7 @@ void lose(){
 
 }
 
-void draw(){
+void draw(int nbr_cartes){
 
 }
 
