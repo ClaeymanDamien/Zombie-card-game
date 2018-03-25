63c59e1ac5c23db8e1957d04f23d66c45fd480f0
DClaeyman <damien.claeyman.20160288@efrei.net>
2018-03-25 16:08:03 +0200

    Fonction d'initialisation

--- main.cpp
+++ main.cpp
@@ -22,48 +22,7 @@ int main()
 
     srand (time(NULL));
 
-    // Deck de base
-    for (int i = 0; i < 5; i++){
-        Lancepierre *lcp1=new Lancepierre;
-        Bouclier *bcl1=new Bouclier;
-        deck.push_back(lcp1);
-        deck.push_back(bcl1);
-    }
-
-    // Pool d'ennemis
-
-    Entite *enmy1 = new Medecin;
-    poolennemis.push_back(enmy1);
-    Entite *enmy2 = new Soldat;
-    poolennemis.push_back(enmy2);
-    Entite *enmy3 = new Infecte;
-    poolennemis.push_back(enmy3);
-    Entite *enmy4 = new Brute;
-    poolennemis.push_back(enmy4);
-    Entite *enmy5 = new Contagieux;
-    poolennemis.push_back(enmy5);
-    Entite *enmy6 = new Brute;
-    poolennemis.push_back(enmy6);
-    Entite *enmy7 = new Exploseur;
-    poolennemis.push_back(enmy7);
-
-    // Pool de cartes
-    Lancepierre *chx1 = new Lancepierre;
-    Medkit *chx2 = new Medkit;
-//    Grenade *chx3 = new Grenade;
-    Bouclier *chx4 = new Bouclier;
-    Steroide *chx5 = new Steroide;
-    poolcartes.push_back(chx1);
-    poolcartes.push_back(chx2);
-    //poolcartes.push_back(chx3);
-    poolcartes.push_back(chx4);
-    poolcartes.push_back(chx5);
-
-    // Ajout des ennemis
-    for (int i = 0; i < 2; i++){
-        Entite *enmy1=new Entite;
-        ennemis.push_back(enmy1);
-    }
+    init_game(player,choixennemis,poolennemis,deck,main,defausse,poolcartes,choixcarte);
 
     while(1){
     create_card_choice(poolcartes,choixcarte);
